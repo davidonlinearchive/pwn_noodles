70aa7313c9127f1cd040f2e0f9df6aefd2ec8f37
dob24 <dob24@njit.edu>
2025-04-19 00:12:25 -0400

    updated script

--- pwn_noodles.sh
+++ pwn_noodles.sh
@@ -37,50 +37,58 @@ fi
 source "$VENV_DIR/bin/activate"
 export PATH="$VENV_DIR/bin:$PATH"
 
+# install all
+install_all(){
+	install_pwntools
+	install_ghidra
+	install_checksec
+	install_pwndbg
+}
+
 # pwntools
 install_pwntools(){
 	echo  "Installing pwn tools into venv at $VENV_DIR"
 	apt-get install software-properties-common
 	apt-add-repository ppa:pwntools/binutils
-	apt-get update
+
 	apt-get install binutils-$ARCH-linux-gnu
-	
 	python3 -m pip install --upgrade pip
 	python3 -m pip install --upgrade pwntools
 }
 
-
 # ghidra
 install_ghidra(){
-	
+	snap install ghidra
 }
 
+# checksec
+install_checksec(){
+	pip install checksec
+}
 
-# ida
+# pwn-dbg
 
+install_pwndbg(){
+	git clone https://github.com/pwndbg/pwndbg 
+	pwndbg/setup.sh
+}
 
-
-# check sec
-
-# pwn gdb
 
 #  === MAIN MENU ===
 echo "===PWN NOODLES MENU==="
 echo "0) install all"
 echo "1) install pwntools"
 echo "2) install ghidra"
-echo "3) install ida"
-echo "4) install checksec"
-echo "5) install pwn-gdb"
+echo "3) install checksec"
+echo "4) install pwn-gdb"
 echo "======================"
-read -p "Selection an option (0-5): " choice
+read -p "Selection an option (0-4): " choice
 
 case $choice in
 	0) install_all ;;
 	1) install_pwntools ;;
 	2) install_ghidra ;;
-	3) install_ida ;;
-	4) install_checksec ;;
-	5) install_pwngdb ;;
+	3) install_checksec ;;
+	4) install_pwndbg ;;
 	*) echo "Invalid response";;
 esac
